8e04947e6909fb025df2ead9a43c956bcb38bee1
MACBOOK <macbookfajri@MACBOOKs-MacBook-Pro.local>
2024-09-02 13:40:10 +0700

    Tambah hehe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,19 @@
 import React from 'react';
 import './App.css';
 import ImageUploader from './components/ImageUploader';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
+    <div>
+      <Helmet>
+        <title>WebP Converter</title>
+        <meta name="description" content="jpg to webp converter" />
+      </Helmet>
+      
     <div className="App">
       <ImageUploader />
+    </div>
     </div>
   );
 }
